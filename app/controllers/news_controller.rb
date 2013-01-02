11110799e4baa12148d23580f88c622f033049fd
Warm <warm@intropica,com>
2013-01-02 17:00:31 +0700

    home completed

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,7 +1,30 @@
 class NewsController < ApplicationController
 
   def index
+    @news = [
+      OpenStruct.new(:first_name => "George" ,:text => "Hello Kitty",
+        :comments => [
+            OpenStruct.new(:first_name => "Jill", :text => "So cute"),
+            OpenStruct.new(:first_name => "Tom", :text => "I don't think so")
+                     ]
+      ),
+      OpenStruct.new(:first_name => "Bill", :text => "Yeahh...!",
+        :comments => [
+            OpenStruct.new(:first_name => "Ali", :text => "What happen?")
+                     ]
+      )
+    ]
+
+
+
+
     # @news = current_user.news
   end
 
 end
+
+
+
+
+
+
