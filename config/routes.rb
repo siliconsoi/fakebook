3a9bd28ef2e17667f5d6c9cb67415c4472ee49bd
Warm <warm@intropica,com>
2013-01-02 11:07:10 +0700

    pull

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Fakebook::Application.routes.draw do
     # resources :daily_summary, :only => [:index]
     # resources :reports, :only => [:index]
     # root :to => 'devise#login'
-    # resources :news, :only => [:index]
-    # root :news => 'news#index'
+    resources :news, :only => [:index]
+    root :news => 'news#index'
 end
