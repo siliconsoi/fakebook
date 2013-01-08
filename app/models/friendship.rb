96d27bd130a6a498bb86d831c6cf89830ebb2c88
tour <tour@intropica.com>
2013-01-08 10:40:32 +0700

    Added new many friends

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,6 +1,7 @@
 class Friendship < ActiveRecord::Base
   attr_accessible :friend_id, :user_id
   belongs_to :user
+  belongs_to :friend, :class_name => "User"
 
   def self.friend(user)
     friends = Friendship.find_all_by_user_id(user)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ActiveRecord::Base
   has_many :friendships
   has_many :posts
-  has_many :friends, :source => :friendship , :through => :friendships
-  # has_many :friends, :through => :friendships
+  has_many :friends, :through => :friendships
+  has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
+  has_many :inverse_friends, :through => :inverse_friendships, :source => :user
+  # has_many :friends, :source => :friendship , :through => :friendships, :foreign_key => "friend_id"
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
